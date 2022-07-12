80bac21d04f4045c5d2530166c06fb42ca4e11d9
Daniel <104681854+DaniPlancha@users.noreply.github.com>
2022-07-12 12:45:10 +0300

    Buttons added
    Main button that changes from start to stop, depending on previous value when clicked, and reset button

---
+++ Chronometer/Chronometer/StaticFiles/script.js
@@ -0,0 +1,46 @@
+﻿var hours = document.querySelector('#hours');
+var minutes = document.querySelector('#minutes');
+var seconds = document.querySelector('#seconds');
+var miliseconds = document.querySelector('#miliseconds');
+
+var timer = new easytimer.Timer();
+var mainButton = document.getElementById('main-button-id');
+var resetButton = document.getElementById('reset-button-id');
+
+timer.addEventListener('secondTenthsUpdated', () => {
+    const obj = timer.getTimeValues();
+
+    hours.innerText = String(obj.hours).padStart(2, '0');
+    minutes.innerText = String(obj.minutes).padStart(2, '0');
+    seconds.innerText = String(obj.seconds).padStart(2, '0');
+    miliseconds.innerText = String(obj.secondTenths);
+});
+
+mainButton.addEventListener('click', () => {
+    if (mainButton.className == 'start-button') {
+        mainButton.classList.remove('start-button');
+        mainButton.classList.add('stop-button');
+        timer.start({
+            precision: 'secondTenths'
+        });
+    } else if (mainButton.className == 'stop-button') {
+        mainButton.classList.remove('stop-button');
+        mainButton.classList.add('start-button');
+        timer.pause();
+    }
+});
+
+resetButton.addEventListener('click', () => {
+    timer.reset();
+    timer.stop();
+
+    if (mainButton.className == 'stop-button') {
+        mainButton.classList.remove('stop-button');
+        mainButton.classList.add('start-button');
+    }
+
+    hours.innerText = '00';
+    minutes.innerText = '00';
+    seconds.innerText = '00';
+    miliseconds.innerText = '0';
+});
