16fbcffbfe1320610d558d643daa4dec6c193f9b
Daniel <104681854+DaniPlancha@users.noreply.github.com>
2022-07-13 14:32:38 +0300

    Adding new chronometers button added
    New timers can be started separately and are completely independant from each other. Functionality like Start, Stop and Reset are also done

--- Chronometer/Chronometer/StaticFiles/script.js
+++ Chronometer/Chronometer/StaticFiles/script.js
@@ -1,46 +1,77 @@
-﻿var hours = document.querySelector('#hours');
-var minutes = document.querySelector('#minutes');
-var seconds = document.querySelector('#seconds');
-var miliseconds = document.querySelector('#miliseconds');
+﻿var timersDictionary = {};
+var counter = 1;
 
-var timer = new easytimer.Timer();
-var mainButton = document.getElementById('main-button-id');
-var resetButton = document.getElementById('reset-button-id');
+function RefreshTimerValues(timer, chronoId) {
+    timer.addEventListener('secondTenthsUpdated', () => {
 
-timer.addEventListener('secondTenthsUpdated', () => {
-    const obj = timer.getTimeValues();
+        let minutesElement = document.querySelector(`#minutes${chronoId}`);
+        let secondsElement = document.querySelector(`#seconds${chronoId}`);
+        let milisecondsElement = document.querySelector(`#miliseconds${chronoId}`);
 
-    hours.innerText = String(obj.hours).padStart(2, '0');
-    minutes.innerText = String(obj.minutes).padStart(2, '0');
-    seconds.innerText = String(obj.seconds).padStart(2, '0');
-    miliseconds.innerText = String(obj.secondTenths);
-});
+        const obj = timer.getTimeValues();
+        minutesElement.innerText = String(obj.minutes).padStart(2, '0');
+        secondsElement.innerText = String(obj.seconds).padStart(2, '0');
+        milisecondsElement.innerText = String(obj.secondTenths);
+    });
+}
 
-mainButton.addEventListener('click', () => {
-    if (mainButton.className == 'start-button') {
-        mainButton.classList.remove('start-button');
-        mainButton.classList.add('stop-button');
+function StartOrStopTimer(element) {
+    let currentIdNum = element.dataset.cid;
+
+    let timer = timersDictionary[currentIdNum];
+
+    if (element.className == 'start-button') {
+
+        element.classList.remove('start-button');
+        element.classList.add('stop-button');
         timer.start({
             precision: 'secondTenths'
         });
-    } else if (mainButton.className == 'stop-button') {
-        mainButton.classList.remove('stop-button');
-        mainButton.classList.add('start-button');
+
+    } else if (element.className == 'stop-button') {
+
+        element.classList.remove('stop-button');
+        element.classList.add('start-button');
         timer.pause();
     }
-});
+}
 
-resetButton.addEventListener('click', () => {
+function ResetTimer(element) {
+    let currentIdNum = element.dataset.cid;
+    let timer = timersDictionary[currentIdNum];
+    let minutesElement = document.querySelector(`#minutes${currentIdNum}`);
+    let secondsElement = document.querySelector(`#seconds${currentIdNum}`);
+    let milisecondsElement = document.querySelector(`#miliseconds${currentIdNum}`);
+
+    let main_button = document.querySelector('#main-button-id' + currentIdNum);
+
     timer.reset();
     timer.stop();
 
-    if (mainButton.className == 'stop-button') {
-        mainButton.classList.remove('stop-button');
-        mainButton.classList.add('start-button');
+    if (main_button.className == 'stop-button') {
+        main_button.classList.remove('stop-button');
+        main_button.classList.add('start-button');
     }
 
-    hours.innerText = '00';
-    minutes.innerText = '00';
-    seconds.innerText = '00';
-    miliseconds.innerText = '0';
-});
+    minutesElement.innerText = '00';
+    secondsElement.innerText = '00';
+    milisecondsElement.innerText = '0';
+}
+
+var AddHtmlTags = () => {
+    document.body.innerHTML +=
+        `<div>
+            <p id="timer-info-paragraph${counter}">
+                <span id="minutes${counter}">00</span> :
+                <span id="seconds${counter}">00</span> :
+                <span id="miliseconds${counter}">0</span>
+            </p >
+            <button id="main-button-id${counter}" data-cid=${counter} class="start-button" onclick="StartOrStopTimer(this)"></button>
+            <button id="reset-button-id${counter}" data-cid=${counter} class="reset-button" onclick="ResetTimer(this)">Reset</button>
+        </div >`;
+
+    timersDictionary[counter] = new easytimer.Timer();
+
+    RefreshTimerValues(timersDictionary[counter], counter);
+    counter++;
+}
